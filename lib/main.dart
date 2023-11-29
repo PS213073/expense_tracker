b8e423ffec86e9e932941fa2ac95e76679f5ffe8
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-11-29 22:56:58 +0100

    adding add bar with title and iconbutton

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
-import 'package:expense_tracker/widgets/expense.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
-      home: Expenses(),
+      home: const Expenses(),
     ),
   );
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -25,9 +25,15 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Expense Tracker"),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
       body: Column(
         children: [
           const Text("The Chart"),
