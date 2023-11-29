b925498962e26c0ba435ca550475df2920607b26
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-11-29 21:56:54 +0100

    expense model updated with enum category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,15 +2,19 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
-  }) : id = uuid.v4();    //this generates unique id
+    required this.category,
+  }) : id = uuid.v4(); //this generates unique id
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
