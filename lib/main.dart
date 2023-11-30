befa1d5cfad35467b6ce84e2de786d7c0bd5876c
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-11-30 17:09:38 +0100

    Dark theme added

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,26 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer),
+        ),
+      ),
       theme: ThemeData.light().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -32,6 +49,7 @@ void main() {
             ),
       ),
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
