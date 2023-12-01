fab29a3604ff602bdfff0dae29a52d348d06ee12
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2023-12-02 00:43:48 +0100

    add ExpenseBucket class that sums up all the expenses in expense list

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -33,3 +33,24 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      // sum = sum + expense.amount;
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
